4b7c8f58e274f2dad6555b5c9acd7eb5d570cba9
Olli Etuaho <olli.etuaho@gmail.com>
2015-08-22 20:07:21 +0300

    Reduce reallocation in soft body renderer

--- softbodyrenderer.js
+++ softbodyrenderer.js
@@ -5,7 +5,7 @@ var SoftBodyRenderer = function(gl, src) {
     this.gl = gl;
     this.vertexBuffer = gl.createBuffer();
     this.texCoordBuffer = gl.createBuffer();
-    
+    this.positionData = null;
 };
 
 /**
@@ -31,28 +31,41 @@ SoftBodyRenderer.prototype.render = function(grid) {
     
     SoftBodyRenderer.shader.use({'u_tex': this.testSprite.texture});
     
-    var arr = [];
-    var texArr = [];
-    var triangleCount = 0;
+    var triangleCount = 2 * grid.width * grid.height;
+    var vertexCount = triangleCount * 3;
+    var texCoordData = null;
+    if (this.positionData == null || vertexCount * 2 != this.positionData.length) {
+        this.positionData = new Float32Array(vertexCount * 2);
+        texCoordData = new Float32Array(vertexCount * 2);
+    }
+    var arrInd = 0;
     for (var x = 0; x < grid.width; ++x) {
         for (var y = 0; y < grid.height; ++y) {
             // Should do with an index buffer...
-            SoftBodyRenderer.pushGridCoords(arr, texArr, grid, x, y);
-            SoftBodyRenderer.pushGridCoords(arr, texArr, grid, x + 1, y);
-            SoftBodyRenderer.pushGridCoords(arr, texArr, grid, x, y + 1);
-            SoftBodyRenderer.pushGridCoords(arr, texArr, grid, x, y + 1);
-            SoftBodyRenderer.pushGridCoords(arr, texArr, grid, x + 1, y);
-            SoftBodyRenderer.pushGridCoords(arr, texArr, grid, x + 1, y + 1);
-            triangleCount += 2;
+            SoftBodyRenderer.pushGridCoords(this.positionData, texCoordData, arrInd, grid, x, y);
+            arrInd += 2;
+            SoftBodyRenderer.pushGridCoords(this.positionData, texCoordData, arrInd, grid, x + 1, y);
+            arrInd += 2;
+            SoftBodyRenderer.pushGridCoords(this.positionData, texCoordData, arrInd, grid, x, y + 1);
+            arrInd += 2;
+            SoftBodyRenderer.pushGridCoords(this.positionData, texCoordData, arrInd, grid, x, y + 1);
+            arrInd += 2;
+            SoftBodyRenderer.pushGridCoords(this.positionData, texCoordData, arrInd, grid, x + 1, y);
+            arrInd += 2;
+            SoftBodyRenderer.pushGridCoords(this.positionData, texCoordData, arrInd, grid, x + 1, y + 1);
+            arrInd += 2;
         }
     }
     gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(arr), gl.STATIC_DRAW);
+    gl.bufferData(gl.ARRAY_BUFFER, this.positionData, gl.DYNAMIC_DRAW);
     var positionAttribLocation = 0;
     gl.vertexAttribPointer(positionAttribLocation, 2, gl.FLOAT, false, 0, 0);
-    var texCoordAttribLocation = 1;
+
     gl.bindBuffer(gl.ARRAY_BUFFER, this.texCoordBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(texArr), gl.STATIC_DRAW);
+    if (texCoordData != null) {
+        gl.bufferData(gl.ARRAY_BUFFER, texCoordData, gl.DYNAMIC_DRAW);
+    }
+    var texCoordAttribLocation = 1;
     gl.vertexAttribPointer(texCoordAttribLocation, 2, gl.FLOAT, false, 0, 0);
     gl.drawArrays(gl.TRIANGLES, 0, triangleCount * 3);
 };
@@ -62,12 +75,14 @@ SoftBodyRenderer.getGridPosition = function(grid, xInd, yInd) {
     return grid.positions[positionsIndex];
 };
 
-SoftBodyRenderer.pushGridCoords = function(target, targetTexCoords, grid, xInd, yInd) {
+SoftBodyRenderer.pushGridCoords = function(target, targetTexCoords, arrInd, grid, xInd, yInd) {
     var pos = SoftBodyRenderer.getGridPosition(grid, xInd, yInd);
-    target.push(pos.x);
-    target.push(pos.y);
-    targetTexCoords.push(xInd / grid.width);
-    targetTexCoords.push(yInd / grid.height);
+    target[arrInd] = (pos.x);
+    target[arrInd + 1] = (pos.y);
+    if (targetTexCoords != null) {
+        targetTexCoords[arrInd] = (xInd / grid.width);
+        targetTexCoords[arrInd + 1] = (yInd / grid.height);
+    }
 };
 
 SoftBodyRenderer.vertexSrc = [
